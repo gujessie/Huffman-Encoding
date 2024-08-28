c02b7e9a0d634d166981d2b549cf1122eba26dd4
gujessie <152340190+gujessie@users.noreply.github.com>
2024-08-28 16:47:36 -0400

    Update Floats_Huffman_Encoding.h

--- Floats_Huffman_Encoding.h
+++ Floats_Huffman_Encoding.h
@@ -2,35 +2,36 @@
 #define FLOATS_HUFFMAN_ENCODING_H
 
 #include <stdio.h>
-#include <queue>     
+#include <queue> 
+#include <vector>    
 #include <stdbool.h>
 
 #define MAXLENGTH 128 * 1024 * 1024
 
 
 /* struct that represents a letter */
-struct node{
-   int index;
-   int frq;
-   struct node *left;
-   struct node *right;
-   unsigned int encode;
-   int encode_length;
- 
+struct Node {
+    int index; // The bucket or value this node represents
+    int freq;  // Frequency of the bucket or value
+    Node* left;  // Left child in the Huffman tree
+    Node* right; // Right child in the Huffman tree
+    unsigned int encode;
+    int encode_length;
 
-   bool operator<(const struct node& comp_node) const {
-        if (frq > comp_node.frq) {
-            return true;
-        }
-        return false;
+    Node(int idx, int frequency) : index(idx), freq(frequency), left(NULL), right(NULL) {}
+};
+
+
+struct LessThanByCnt {
+    bool operator()(const Node* lhs, const Node* rhs) const {
+        return lhs->freq > rhs->freq;  // Min-heap based on frequency
     }
 };
-typedef struct node Node;
 
 int quantize(float* src, size_t size, float error, int* quantized_src, float* min);
 void record_frequencies(const int* quantized_src, int fsize, int num_buckets, int* frqs);
-void make_queue(std::priority_queue<Node> &tree, int *frqs, int num_buckets);
-Node* build_tree(std::priority_queue<Node> &tree);
+void make_queue(std::priority_queue<Node*, std::vector<Node*>, LessThanByCnt>& phtree, int* frqs, int num_buckets);
+Node* build_tree(std::priority_queue<Node*, std::vector<Node*>, LessThanByCnt>& tree);
 void assign_encode(Node *root);
 void store_encodings_helper(Node* root, int encodings[][2], int num_buckets);
 void compress(const int* src, char* dest, int fsize, int* destsize, int encodings[][2]);
